d8192c0b70032835bdfdeaf815e53769ce881959
trixxed <marcthorgersen@gmail.com>
2018-10-17 15:08:37 +0200

    why cache no ignore

--- haas_site/urls.py
+++ haas_site/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index_url'),
-    path('plants/', views.plant, name='plant_url'),
-    path('lights/', views.light, name='light_url'),
+    path('plants/', views.plant_overview, name='plant_url'),
+    path('plants/<int:plant_id>/', views.plant, name='plant_specific'),
+    path('lights/', views.light_overview, name='light_url'),
 ]
